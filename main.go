5cb4fe3ec84c56e4235c7433153bb17d391ead23
chieund <chieund.fat@gmail.com>
2022-08-28 00:29:39 +0700

    master: add demo example

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/typesense/typesense-go/typesense"
 	"github.com/typesense/typesense-go/typesense/api"
+	"os"
 )
 
 func main() {
@@ -40,8 +41,8 @@ func main() {
 		NumEmployees int    `json:"num_employees"`
 		Country      string `json:"country"`
 	}{
-		ID:           "123",
-		CompanyName:  "Stark Industries",
+		ID:           "1234",
+		CompanyName:  "Stark Industries 111",
 		NumEmployees: 5215,
 		Country:      "USA",
 	}
@@ -50,9 +51,61 @@ func main() {
 	fmt.Println(result)
 
 	searchParameters := &api.SearchCollectionParams{
-		Q: "stark",
+		Q:       "stark",
+		QueryBy: "company_name",
 	}
 
+	// search result
 	result1, _ := client.Collection("companies").Documents().Search(searchParameters)
-	fmt.Println(result1)
+	for _, value := range *result1.Hits {
+		document := value.Document
+		fmt.Println((*document)["company_name"], (*document)["num_employees"])
+	}
+
+	// get document by id
+	result2, _ := client.Collection("companies").Document("123").Retrieve()
+	fmt.Println(result2)
+
+	// get all collection
+	fmt.Println(client.Collection("companies").Retrieve())
+
+	// export collection
+	client.Collection("companies").Documents().Export()
+
+	// import
+	documents := []interface{}{
+		struct {
+			ID           string `json:"id"`
+			CompanyName  string `json:"company_name"`
+			NumEmployees int    `json:"num_employees"`
+			Country      string `json:"country"`
+		}{
+			ID:           "1236",
+			CompanyName:  "Stark Industries",
+			NumEmployees: 5215,
+			Country:      "USA",
+		},
+	}
+
+	action := "create"
+	batchSize := 40
+	params := &api.ImportDocumentsParams{
+		Action:    &action,
+		BatchSize: &batchSize,
+	}
+	client.Collection("companies").Documents().Import(documents, params)
+
+	fmt.Println("import file json")
+	action = "create"
+	batchSize = 40
+	params = &api.ImportDocumentsParams{
+		Action:    &action,
+		BatchSize: &batchSize,
+	}
+	importBody, err := os.Open("documents.jsonl")
+	fmt.Println(importBody, err)
+	client.Collection("companies").Documents().ImportJsonl(importBody, params)
+
+	// list Collection
+	fmt.Println(client.Collections().Retrieve())
 }
